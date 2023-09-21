42b469bc896eaed7da5c4a194e6bdcdf8dbb6a06
shegolenoks <94798975+shegolenoks@users.noreply.github.com>
2023-09-21 23:58:53 +0300

    Add files via upload
    binary search

---
+++ c++ N1.cpp
@@ -0,0 +1,32 @@
+﻿#include<bits/stdc++.h>
+#include <vector>
+#define ll long long
+#define pb push_back
+#define vec vector
+using namespace std;
+int main()
+{
+    int n, item;
+    cin >> n >> item;
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    int low = 0;
+    int high = n - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (item < a[mid]) {
+            high = mid - 1;
+        }
+        else if (item > a[mid]) {
+            low = mid + 1;
+        }
+        else {
+            cout << mid;
+            return 0;
+        }
+    }
+    cout << "Null";
+    return 0;
+}
